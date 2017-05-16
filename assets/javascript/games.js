8652c474efbfb5d8514bf2e1d0b1a758a02ad9f5
HayelomR <rhayelom@gmail.com>
2017-05-16 14:06:05 -0600

    added some code

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,7 +1,22 @@
+/* words that will be used for the game.*/
 var words =  [ "Bugatti","lumborghini","Tesla"
 "Audi", "Jaguar","lexus","Mercedes", "Cadillaic",
- "Bmw","Acura","Maserati", "land rover","Jeep"," acura"
+"Bmw","Acura","Maserati", "land rover","Jeep"," acura"
 ,"kia"];
 
+//Variables for tracking our wins, currentwords and gussedWords. They begin at 0.
+      var randomNumber = Math.floor(Math.random() * 15);// the random # that will be selected
+      var gusesRemaining = 0;
+      var currentWords = 0;
+      var gussedWords = 0;
+      document.onkeyup = function(event) { // this tells the player to use a keyboard keys
+
+      	var userGuess = event.key;
 
 
+
+
+
+
+
+      }
