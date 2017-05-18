6133aaeb9432a777193a9c48ef4fdbd6b65f673f
HayelomR <rhayelom@gmail.com>
2017-05-17 18:26:34 -0600

    added few codes to js

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,22 +1,35 @@
 /* words that will be used for the game.*/
-var words =  [ "Bugatti","lumborghini","Tesla"
+var cars =  [ "Bugatti","lumborghini","Tesla"
 "Audi", "Jaguar","lexus","Mercedes", "Cadillaic",
 "Bmw","Acura","Maserati", "land rover","Jeep"," acura"
 ,"kia"];
 
 //Variables for tracking our wins, currentwords and gussedWords. They begin at 0.
-      var randomNumber = Math.floor(Math.random() * 15);// the random # that will be selected
-      var gusesRemaining = 0;
-      var currentWords = 0;
-      var gussedWords = 0;
-      document.onkeyup = function(event) { // this tells the player to use a keyboard keys
+      var word = cars[Math.floor(Math.random() * cars.length)];// the random # that will be selected
+      from the cars array.
 
-      	var userGuess = event.key;
-
-
-
-
-
-
+      var answerArray = []; // new  array used to hold the answer array
+      for (i = 0; i < word.length; i++) {
+      	answerArray[i] = "_"
 
       }
+var remainingLetters = word.length // creates letters that remain to kbe guessed.
+if (remainingLetters>0) {
+
+} else {
+
+}
+document.onkeyup = function(event) {
+	var userGuess = event.key;
+	for(j= 0; j < word.length; j++){
+		if(word[j]) === guess
+			answerArray[j] = guess;
+		remainingLetters-1
+	}
+}
+
+
+
+
+}
+
